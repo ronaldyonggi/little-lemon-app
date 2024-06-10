9c2f1912593a8485f7c0094ec2e204194865e92a
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-06-10 18:59:00 +0800

    Welcome component split to multiple modules

--- components/Welcome/index.jsx
+++ components/Welcome/index.jsx
@@ -20,33 +20,3 @@ export default function Welcome() {
     </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#495e57',
-  },
-  alternativeContainer: {
-    flex: 1,
-    backgroundColor: '#333333',
-  },
-  heading: {
-    color: 'white',
-    fontSize: 40,
-    textAlign: 'center',
-    padding: 40,
-  },
-  subheading: {
-    color: 'white',
-    fontSize: 25,
-    textAlign: 'center',
-    padding: 20,
-    marginVertical: 8,
-  },
-  nameInput: {
-    backgroundColor: '#eceaea',
-    fontSize: 30,
-    marginHorizontal: 15,
-    padding: 15,
-  },
-});

37156415b126e09cc32657febb505b0e3b0d4ca6
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-06-13 10:57:30 +0800

    incorporate Navigation to App

--- App.js
+++ App.js
@@ -1,28 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Welcome from './components/Welcome';
+import WelcomeScreen from './screen/WelcomeScreen';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import FlatListMenu from './components/FlatListMenu';
 import SectionListMenu from './components/SectionListMenu';
 import SectionListMenuWithPrice from './components/SectionListMenuWithPrice';
 import FeedbackForm from './components/FeedbackForm';
-import LoginScreen from './components/LoginScreen';
+import LoginScreen from './screen/LoginScreen';
 import WelcomeWithImage from './components/WelcomeWithImage';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer style={styles.alternativeContainer}>
       <Header />
-      <Welcome />
-      {/* <FlatListMenu /> */}
-      {/* <Footer /> */}
-      {/* <SectionListMenu /> */}
-      {/* <SectionListMenuWithPrice /> */}
-      {/* <FeedbackForm /> */}
-      {/* <LoginScreen /> */}
-      {/* <WelcomeWithImage /> */}
+      <Stack.Navigator
+        initialRouteName='Welcome'
+        screenOptions={{
+          headerTitleAlign: 'center',
+        }}
+      >
+        <Stack.Screen name='Login' component={LoginScreen} />
+        <Stack.Screen name='Welcome' component={WelcomeScreen} />
+        {/* <Welcome /> */}
+        {/* <FlatListMenu /> */}
+        {/* <SectionListMenu /> */}
+        {/* <SectionListMenuWithPrice /> */}
+        {/* <FeedbackForm /> */}
+        {/* <LoginScreen /> */}
+        {/* <WelcomeWithImage /> */}
+      </Stack.Navigator>
+      <Footer />
     </NavigationContainer>
   );
 }
